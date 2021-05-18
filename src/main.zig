81c32405b47a7a885efb449fdb753ee78b6a218a
Zooce <zooce@hey.com>
2021-05-18 09:23:21 -0700

    initial commit

---
+++ src/main.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn main() anyerror!void {
+    // TODO: open first argument as file
+
+    // TODO: if no first argument, read from stdin
+
+    readloop: while(true) {
+        // TODO: read first line or stdin to eol
+
+        // TODO: if nothing left to read, break
+    }
+
+    // TODO: traverse AST and do variable substitution
+
+    // TODO: write AST to a JSON file
+}
